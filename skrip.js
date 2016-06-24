768e462045eeb7bc9276fe3ebbea0ace5fd2745b
Chris Gardner <ctgardner89@gmail.com>
2016-06-24 13:34:23 -0400

    slight tweak, diff tag search

--- skrip.js
+++ skrip.js
@@ -1,19 +1,20 @@
 $(document).ready( function() {
 
   $("button").click(function(){
-    var rl = "http://api.giphy.com/v1/gifs/trending?q=funny&limit=45&api_key=dc6zaTOxFJmzC";
+    var rl = "http://api.giphy.com/v1/gifs/search?q=fail&limit=45&api_key=dc6zaTOxFJmzC";
     $.ajax({
       url: rl,
       type: "GET",
       dataType: "json"
     }).done(function(response){
-      setInterval(function(){
-      var apimgs = [];
-      var i = Math.floor(Math.round(Math.random()*45))
-      var test = response.data[i].images.original.url;
-      apimgs.push(test);
-      $("div").html("<img src="+apimgs[apimgs.length-1]+">");
-    },600)
+        setInterval(function(){
+        var apimgs = [];
+        var i = Math.floor(Math.round(Math.random()*33))
+        var test = response.data[i].images.original.url;
+        apimgs.push(test);
+        $("div").html("<img src="+apimgs[apimgs.length-1]+">");
+      },3900)
+    console.log(response)
     }).fail(function(response){
       console.log("You can't believe, that is why you fail")
     }).always(function(){
